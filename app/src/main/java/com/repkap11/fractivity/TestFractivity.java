aa1090f9f813c47c1b6697be65f0e1b430018867
Paul Repka <repkap11@gmail.com>
2016-02-16 16:16:51 -0500

    Initial Commit. with working example.
    Signed-off-by: Paul Repka <repkap11@gmail.com>

---
+++ app/src/main/java/com/repkap11/fractivity/TestFractivity.java
@@ -0,0 +1,54 @@
+package com.repkap11.fractivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class TestFractivity extends Fractivity {
+    private TestFractivityFragment mTestFragment;
+
+    @Override
+    protected void fragmentCreated(FractivityFragment fractivityFragment) {
+        mTestFragment = (TestFractivityFragment) fractivityFragment;
+    }
+
+    @Override
+    protected FractivityFragment createFragment(Bundle savedInstanceState) {
+        FractivityFragment fragment = new TestFractivityFragment();
+        //use the bundle to create the fragment
+        return fragment;
+    }
+
+    public static final class TestFractivityFragment extends FractivityFragment {
+        int mPersistingVar = 0;
+
+        @Override
+        protected void create(Bundle savedInstanceState) {
+            mPersistingVar = 76;
+        }
+
+        TextView mTextView;
+
+        @Override
+        protected View createView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fractivity_test, container, false);
+            mTextView = (TextView) rootView.findViewById(R.id.fractivity_test_text);
+            incrementCounter();
+
+            return rootView;
+        }
+
+        public void incrementCounter() {
+            mPersistingVar += 1;
+            mTextView.setText("" + mPersistingVar);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        mTestFragment.incrementCounter();
+    }
+}
